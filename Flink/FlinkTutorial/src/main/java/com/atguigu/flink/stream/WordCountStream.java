d2a758c722824b3aec9ebf91c177e1eabe3303e8
forrestlmj <18810276930@163.com>
2021-02-15 20:41:06 +0800

    继续第四章的笔记

--- Flink/FlinkTutorial/src/main/java/com/atguigu/flink/stream/WordCountStream.java
+++ Flink/FlinkTutorial/src/main/java/com/atguigu/flink/stream/WordCountStream.java
@@ -5,6 +5,7 @@ import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 
 
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
@@ -16,11 +17,16 @@ public class WordCountStream {
         
         // 设置并行度，默认值=当前计算机的CPU逻辑核心数（设置成1就是单线程）
 //        env.setMaxParallelism(32);
-        
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String host = parameterTool.get("host");
+        int port = parameterTool.getInt("port");
+
         // 从文本中读取数据
-        String inputPath = "src/main/resources/hello.txt";
-        DataStream<String> inputDataStream = env.readTextFile(inputPath);
-        
+//        String inputPath = "src/main/resources/hello.txt";
+//        DataStream<String> inputDataStream = env.readTextFile(inputPath);
+
+        // 从socket文本流中读取数据
+        DataStream<String> inputDataStream = env.socketTextStream(host, port);
         // 基于流进行转换计算
         DataStream<Tuple2<String,Integer>> resultStream =
                 inputDataStream.flatMap(new WordCount.MyFlatMapper())
