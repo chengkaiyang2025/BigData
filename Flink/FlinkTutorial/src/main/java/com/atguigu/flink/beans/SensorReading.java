7b584bb3ad89a19d3e5f08ec30fcedf2b1d6ca2b
forrestlmj <18810276930@163.com>
2021-02-15 23:14:46 +0800

    1、增加从env.Collection()中添加元素的方法。 2、pom中的依赖从flink-streaming-scala改为flink-streaming-java

---
+++ Flink/FlinkTutorial/src/main/java/com/atguigu/flink/beans/SensorReading.java
@@ -0,0 +1,52 @@
+package com.atguigu.flink.beans;
+
+import scala.Int;
+
+public class SensorReading {
+    private String name;
+    private Long timeStamp;
+
+    @Override
+    public String toString() {
+        return "SensorReading{" +
+                "name='" + name + '\'' +
+                ", timeStamp=" + timeStamp +
+                ", tem=" + tem +
+                '}';
+    }
+
+    public SensorReading() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public double getTem() {
+        return tem;
+    }
+
+    public void setTem(double tem) {
+        this.tem = tem;
+    }
+
+    public SensorReading(String name, Long timeStamp, double tem) {
+        this.name = name;
+        this.timeStamp = timeStamp;
+        this.tem = tem;
+    }
+
+    private double tem;
+}
