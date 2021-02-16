a7f2f7380d9df1405efc19552f9365ddd3ba6f00
forrestlmj <18810276930@163.com>
2021-02-16 12:07:18 +0800

    1、从文件中读取数据源

---
+++ Flink/FlinkTutorial/src/main/java/com/atguigu/flink/stream/SourceTest2_Collection.java
@@ -0,0 +1,29 @@
+package com.atguigu.flink.stream;
+
+import com.atguigu.flink.beans.SensorReading;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class SourceTest2_Collection {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+
+        String filePath = "src/main/resources/sersor_reading.csv";
+        DataStream<String> input = env.readTextFile(filePath);
+        DataStream<SensorReading> sensorReadingDataStream = input.map(new MapFunction<String, SensorReading>() {
+            @Override
+            public SensorReading map(String s) throws Exception {
+                String[] re = s.split(",");
+                Long t = Long.valueOf(re[1]);
+                String name = re[0];
+                double tem = Double.parseDouble(re[2]);
+                return new SensorReading(name,t,tem);
+            }
+        });
+        sensorReadingDataStream.print("sensor_reading");
+        env.execute("从文件中读取");
+    }
+}
