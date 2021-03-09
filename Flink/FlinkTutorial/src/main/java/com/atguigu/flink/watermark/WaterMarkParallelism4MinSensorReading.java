03ae1bc4b46ff48579ab95769a78ae538696008d
forrestlmj <18810276930@163.com>
2021-03-09 15:08:52 +0800

    并行度为4的情况下 水印传播的规律

---
+++ Flink/FlinkTutorial/src/main/java/com/atguigu/flink/watermark/WaterMarkParallelism4MinSensorReading.java
@@ -0,0 +1,65 @@
+package com.atguigu.flink.watermark;
+
+import com.atguigu.flink.beans.SensorReading;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.OutputTag;
+
+/**
+ * @author ：yangchengkai@yunzhangfang.com
+ * @description：设置并行度为4，用来观测水印在并行度为提高的情况下水印的传播情况
+ * 参考：https://ashiamd.github.io/docsify-notes/#/study/BigData/Flink/%E5%B0%9A%E7%A1%85%E8%B0%B7Flink%E5%85%A5%E9%97%A8%E5%88%B0%E5%AE%9E%E6%88%98-%E5%AD%A6%E4%B9%A0%E7%AC%94%E8%AE%B0?id=%e5%b9%b6%e8%a1%8c%e4%bb%bb%e5%8a%a1watermark%e4%bc%a0%e9%80%92%e6%b5%8b%e8%af%95
+ * @date ：2021/3/9 下午2:18
+ */
+
+
+public class WaterMarkParallelism4MinSensorReading {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.setParallelism(1);
+
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.getConfig().setAutoWatermarkInterval(100);
+
+        // socket文本流
+        DataStream<String> inputStream = env.socketTextStream("localhost", 7777);
+
+        // 转换成SensorReading类型，分配时间戳和watermark
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        })
+
+                // 乱序数据设置时间戳和watermark
+                .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<SensorReading>(Time.seconds(2)) {
+                    @Override
+                    public long extractTimestamp(SensorReading element) {
+                        return element.getTimeStamp() * 1000L;
+                    }
+                });
+
+        OutputTag<SensorReading> outputTag = new OutputTag<SensorReading>("late") {
+        };
+
+        // 基于事件时间的开窗聚合，统计15秒内温度的最小值
+        SingleOutputStreamOperator<SensorReading> minTempStream = dataStream.keyBy("name")
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
+                .allowedLateness(Time.minutes(1))
+                .sideOutputLateData(outputTag)
+                .minBy("tem");
+
+        minTempStream.print("minTemp");
+        minTempStream.getSideOutput(outputTag).print("late");
+        dataStream.print("souce");
+        env.execute();
+    }
+
+}
