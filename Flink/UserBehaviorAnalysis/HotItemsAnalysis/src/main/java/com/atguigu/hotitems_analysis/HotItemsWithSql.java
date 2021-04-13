e2295019ad6d0fefb13636dc49eae5a369a5d7a2
forrestlmj <18810276930@163.com>
2021-04-13 22:08:19 +0800

    使用flink sql 实时计算top10商品

--- Flink/UserBehaviorAnalysis/HotItemsAnalysis/src/main/java/com/atguigu/hotitems_analysis/HotItemsWithSql.java
+++ Flink/UserBehaviorAnalysis/HotItemsAnalysis/src/main/java/com/atguigu/hotitems_analysis/HotItemsWithSql.java
@@ -6,9 +6,13 @@ import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.table.api.EnvironmentSettings;
 //import org.apache.flink.table.api.Table;
 //import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.sources.wmstrategies.AscendingTimestamps;
 import org.apache.flink.types.Row;
 
 public class HotItemsWithSql {
@@ -20,7 +24,7 @@ public class HotItemsWithSql {
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
         // 2、读取数据源
-        String file = "/home/yzf/IdeaProjects/Bigdata/Flink/UserBehaviorAnalysis/HotItemsAnalysis/src/main/resources/UserBehavior.csv";
+        String file = "D:\\IdeaProject\\Bigdata\\Flink\\UserBehaviorAnalysis\\HotItemsAnalysis\\src\\main\\resources\\UserBehavior.csv";
         DataStreamSource<String> source = env.readTextFile(file);
         // 3、转为pojo，时间戳与watermark
         SingleOutputStreamOperator<UserBehavior> map = source.map(new MapFunction<String, UserBehavior>() {
@@ -36,22 +40,36 @@ public class HotItemsWithSql {
                         new Long(split[4])
                 );
             }
+        }).assignTimestampsAndWatermarks(new AscendingTimestampExtractor<UserBehavior>() {
+            @Override
+            public long extractAscendingTimestamp(UserBehavior userBehavior) {
+                return userBehavior.getTimestamp() * 1000L;
+            }
         });
         // 4、创建表执行环境，使用blink版本
-//        EnvironmentSettings settings = EnvironmentSettings.newInstance()
-//                .useBlinkPlanner()
-//                .inStreamingMode()
-//                .build();
+        EnvironmentSettings settings = EnvironmentSettings.newInstance()
+                .useBlinkPlanner()
+                .inStreamingMode()
+                .build();
 //
-//        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env, settings);
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env, settings);
 //        // 5、将流转为表
-//        tableEnv.createTemporaryView("data_table",map,"itemId,behavior,timestamp.rowtime as ts");
+        tableEnv.createTemporaryView("data_table",map,"itemId,behavior,timestamp.rowtime as ts");
 //
-//        Table resultSqltable = tableEnv.sqlQuery("select * from data_table where behavior = 'pv'");
+        Table resultSqltable = tableEnv.sqlQuery("select * from " +
+                "  ( select *, ROW_NUMBER() over (partition by windowEnd order by cnt desc) as row_num " +
+                "  from ( " +
+                "    select itemId, count(itemId) as cnt, HOP_END(ts, interval '5' minute, interval '1' hour) as windowEnd " +
+                "    from data_table " +
+                "    where behavior = 'pv' " +
+                "    group by itemId, HOP(ts, interval '5' minute, interval '1' hour)" +
+                "    )" +
+                "  ) " +
+                " where row_num <= 5 ");
 //
 //        // 6、纯sql实现
-//        tableEnv.toRetractStream(resultSqltable,Row.class).print();
-        map.print();
+        tableEnv.toRetractStream(resultSqltable,Row.class).print();
+//        map.print();
         env.execute();
     }
 }
