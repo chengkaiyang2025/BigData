c71343c7c252bdc805d1421a9080d4849a11e919
forrestlmj <18810276930@163.com>
2021-05-10 22:44:22 +0800

    NginxBean json

--- Hadoop/HadoopDemo/MapReducerPrac/src/main/java/com/atguigu/practice/bean/NginxBean.java
+++ Hadoop/HadoopDemo/MapReducerPrac/src/main/java/com/atguigu/practice/bean/NginxBean.java
@@ -1,4 +1,19 @@
 package com.atguigu.practice.bean;
 
-public class NginxBean {
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class NginxBean implements Writable {
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
