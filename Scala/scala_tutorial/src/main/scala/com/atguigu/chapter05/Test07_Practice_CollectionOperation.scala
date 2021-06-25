c9d96d45749969dfadd4bcb65994bc0c189e32c7
forrestlmj <18810276930@163.com>
2021-06-25 18:39:02 +0800

    数组与操作

--- Scala/scala_tutorial/src/main/scala/com/atguigu/chapter05/Test07_Practice_CollectionOperation.scala
+++ Scala/scala_tutorial/src/main/scala/com/atguigu/chapter05/Test07_Practice_CollectionOperation.scala
@@ -1,5 +1,18 @@
 package com.atguigu.chapter05
 
 object Test07_Practice_CollectionOperation {
-
+  def main(args: Array[String]): Unit = {
+    val arr: Array[Int] = Array(12, 45, 75, 98)
+    // 对数组进行操作，将操作抽象出来，然后返回一个新数组
+    def arrayOperations(arr: Array[Int], func: (Int) => Int):Array[Int] = {
+      for( elem <- arr) yield func(elem)
+    }
+    // 定义一个加一的操作
+    val addOne = (a: Int) => (a + 1)
+    // 调用函数
+    for(e <- arrayOperations(arr, addOne)) println(e)
+    // 调入匿名数据*2
+    println("----------")
+    for(e <- arrayOperations(arr, _ * 2 )) println(e)
+  }
 }
