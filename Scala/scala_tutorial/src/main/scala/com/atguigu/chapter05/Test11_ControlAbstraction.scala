92f5cb48381f6c0e27e146f1bb7fffdcb9e99646
forrestlmj <18810276930@163.com>
2021-06-26 10:56:35 +0800

    test11

--- Scala/scala_tutorial/src/main/scala/com/atguigu/chapter05/Test11_ControlAbstraction.scala
+++ Scala/scala_tutorial/src/main/scala/com/atguigu/chapter05/Test11_ControlAbstraction.scala
@@ -1,5 +1,20 @@
 package com.atguigu.chapter05
 
 object Test11_ControlAbstraction {
-
+  def main(args: Array[String]): Unit = {
+    def f0(a: Int) = {
+      println(s"a:${a}")
+    }
+    println(f0(23))
+    def f1() = {
+      println("f1()調用")
+      12
+    }
+    f0(f1())
+    println("=====================")
+    f0({
+      println("-----f0")
+      23
+    })
+  }
 }
