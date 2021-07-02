bba97009d53b09f2ef51887b6222634cef00620b
forrestlmj <18810276930@163.com>
2021-07-02 22:13:41 +0800

    动态绑定 多态

--- Scala/scala_tutorial/src/main/scala/com/atguigu/chapter06/Test08_DynamicBind.scala
+++ Scala/scala_tutorial/src/main/scala/com/atguigu/chapter06/Test08_DynamicBind.scala
@@ -4,5 +4,26 @@ package com.atguigu.chapter06
  *动态绑定，体现了面向对象中的多态属性。
  */
 object Test08_DynamicBind {
+  def main(args: Array[String]): Unit = {
+    val student: Person8 = new Student8
+    println(student.name)
+    println(student.age)
+    student.printInfo()
+  }
+}
+class Person8{
+  var name: String = "person"
+  var age: Int = 19
 
+  def printInfo():Unit = {
+    println(s"name ${name},age ${age}")
+  }
+}
+class Student8 extends Person8{
+  this.name = "student"
+  this.age = 11
+
+  override def printInfo(): Unit = {
+    println(s"student ${name},${age}")
+  }
 }
