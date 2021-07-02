928501dfa5ce87e1dfe1b686ff6036751c2d0540
forrestlmj <18810276930@163.com>
2021-07-02 22:18:23 +0800

    抽象方法

--- Scala/scala_tutorial/src/main/scala/com/atguigu/chapter06/Test09_AbstractClass.scala
+++ Scala/scala_tutorial/src/main/scala/com/atguigu/chapter06/Test09_AbstractClass.scala
@@ -4,5 +4,24 @@ package com.atguigu.chapter06
  *抽象类
  */
 object Test09_AbstractClass {
+  def main(args: Array[String]): Unit = {
+    val student = new Student9()
+    student.sleep()
+  }
+}
+abstract class Person9{
+  var name: String = "people"
+  var age: Int
 
+  // 非抽象方法
+  def eat(): Unit = {
+    println("people is eating")
+  }
+  // 抽象方法
+  def sleep(): Unit
+}
+class Student9 extends Person9{
+  override var age: Int = 20
+
+  override def sleep(): Unit = {println("sleep at classroom")}
 }
