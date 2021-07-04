219d3602887bc8828741ff84839dc256c6bf9642
forrestlmj <18810276930@163.com>
2021-07-04 15:01:52 +0800

    trait

--- Scala/scala_tutorial/src/main/scala/com/atguigu/chapter06/Test13_Trait.scala
+++ Scala/scala_tutorial/src/main/scala/com/atguigu/chapter06/Test13_Trait.scala
@@ -4,5 +4,42 @@ package com.atguigu.chapter06
  * 特质，就是java里的interface
  */
 object Test13_Trait {
+  def main(args: Array[String]): Unit = {
+    val student1 = new Student13
+    student1.study()
+    student1.dating()
+  }
+}
+class Person13{
+  val name: String = "person"
+  var age: Int = 28
+  def sayHello(): Unit = {
+    println("hello,every one")
+  }
+  def increase(): Unit = {
+    println("age is increasing")
+  }
+}
 
+trait Young{
+  val name: String = "young"
+  var age: Int
+  def play():Unit = {
+    println("young people like play")
+  }
+  def dating():Unit
+}
+
+class Student13 extends Person13 with Young {
+  def study(): Unit = {
+    println("student like study")
+  }
+
+  override val name: String = "student"
+  override def play(): Unit = {
+    println("student like play")
+  }
+  override def dating(): Unit = {
+    println("student go to party and dating")
+  }
 }
--- Scala/scala_tutorial/src/main/scala/com/atguigu/chapter06/Test14_TraitMixin.scala
+++ Scala/scala_tutorial/src/main/scala/com/atguigu/chapter06/Test14_TraitMixin.scala
@@ -4,5 +4,39 @@ package com.atguigu.chapter06
  *实现多个特质时候的掺入问题
  */
 object Test14_TraitMixin {
+  def main(args: Array[String]): Unit = {
+    val student1 = new Student14 with Talent {
+      override def dancing(): Unit = println("dancing")
 
+      override def singing(): Unit = println("singing")
+    }
+    student1.dancing()
+    student1.dating()
+    student1.singing()
+  }
 }
+trait Knowledge{
+  var amount: Int = 0
+  def increase(): Unit
+}
+
+trait Talent{
+  def singing(): Unit
+  def dancing(): Unit
+}
+
+class Student14 extends Person13 with Young with Knowledge{
+  override val name: String = "student"
+  override def dating(): Unit = {
+    println(s"student ${name} is dating.")
+  }
+
+  override def sayHello(): Unit = {
+    println(s"hello from student: ${name}")
+  }
+
+  override def play(): Unit = {
+
+  }
+}
+
