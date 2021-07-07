13e3e20857e99b5882adb67b81191531ccccca6d
forrestlmj <18810276930@163.com>
2021-07-07 09:40:45 +0800

    kebian

--- Scala/scala_tutorial/src/main/scala/com/atguigu/chapter07/Test02_ArrrayBuffer.scala
+++ Scala/scala_tutorial/src/main/scala/com/atguigu/chapter07/Test02_ArrrayBuffer.scala
@@ -1,5 +1,18 @@
 package com.atguigu.chapter07
 
-object Test02_ArrrayBuffer {
+import scala.collection.mutable.ArrayBuffer
 
+object Test02_ArrrayBuffer {
+  def main(args: Array[String]): Unit = {
+    val arr1: ArrayBuffer[Int] = new ArrayBuffer[Int]()
+    val arr2: ArrayBuffer[Int] = ArrayBuffer(23, 57 ,92)
+    println(arr1)
+    println(arr2)
+    println("----- 遍历")
+
+    arr2.foreach((i) => (println(i)))
+    for(i <- arr2.indices){
+      println(s"${i} is ${arr2(i)}")
+    }
+  }
 }
