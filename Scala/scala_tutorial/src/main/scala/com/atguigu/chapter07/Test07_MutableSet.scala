85e62f084af364c3fed1e3abccf8c3e5bf098a24
forrestlmj <18810276930@163.com>
2021-07-10 20:12:51 +0800

    set

--- Scala/scala_tutorial/src/main/scala/com/atguigu/chapter07/Test07_MutableSet.scala
+++ Scala/scala_tutorial/src/main/scala/com/atguigu/chapter07/Test07_MutableSet.scala
@@ -1,5 +1,17 @@
 package com.atguigu.chapter07
 
-object Test07_MutableSet {
+import scala.collection.mutable
 
+object Test07_MutableSet {
+  def main(args: Array[String]): Unit = {
+    val set1: mutable.Set[Int] = mutable.Set(13, 12, 3, 2, 1, -1, 2, 2, 2)
+    println(set1)
+    set1.add(-100000)
+    println(set1)
+    set1+=111
+    println(set1)
+    set1 -= 111
+    set1.remove(13)
+    println(set1)
+  }
 }
