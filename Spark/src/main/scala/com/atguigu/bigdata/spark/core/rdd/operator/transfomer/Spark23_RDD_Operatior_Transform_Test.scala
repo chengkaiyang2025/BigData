9aa5efc59eea10947963a00f7b3dcecc6f9526f8
forrestlmj <18810276930@163.com>
2021-08-01 20:22:11 +0800

    join

---
+++ Spark/src/main/scala/com/atguigu/bigdata/spark/core/rdd/operator/transfomer/Spark23_RDD_Operatior_Transform_Test.scala
@@ -0,0 +1,28 @@
+package com.atguigu.bigdata.spark.core.rdd.operator.transfomer
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ *求出单词最多的
+ */
+object Spark23_RDD_Operatior_Transform_Test {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local[*]").setAppName("RDD")
+    val sc = new SparkContext(conf)
+
+    val rdd = sc.makeRDD(List(
+
+      ("a", 1), ("a", 2), ("b", 3),
+    ))
+    val rdd2 = sc.makeRDD(List(
+      ("a", -2), ("b", -1), ("b", -3)
+    ))
+    rdd.cogroup(rdd2).collect().foreach(println)
+
+    sc.stop()
+
+  }
+
+}
+
+
